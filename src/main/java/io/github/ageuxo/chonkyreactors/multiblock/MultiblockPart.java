44038f01c34854b8db46a8a86a9718a5bc2ae61b
ageuxo <ageuxo@gmail.com>
2023-08-06 21:47:11 +0200

    Changed MultiblockPart codec to use vanilla style lists of valid blocks

--- src/main/java/io/github/ageuxo/chonkyreactors/multiblock/MultiblockPart.java
+++ src/main/java/io/github/ageuxo/chonkyreactors/multiblock/MultiblockPart.java
@@ -5,22 +5,62 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.BlockUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagEntry;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public record MultiblockPart(String name, List<Block> blocks,
+public record MultiblockPart(String name, List<TagEntry> entries,
                              int minX, int maxX, int minY, int maxY, int minZ, int maxZ) {
+
+    @SuppressWarnings("deprecation")
+    public static MultiblockPart create(String name, List<Block> blocks, int minX, int maxX, int minY, int maxY, int minZ, int maxZ){
+        List<TagEntry> list = new ArrayList<>();
+        blocks.forEach(block -> list.add(TagEntry.element(BuiltInRegistries.BLOCK.getKey(block))));
+        return new MultiblockPart(name, list, minX, maxX, minY, maxY, minZ, maxZ);
+    }
+
+    public static MultiblockPart createFromTags(String name, List<TagKey<Block>> blockTagKeys, int minX, int maxX, int minY, int maxY, int minZ, int maxZ){
+        List<TagEntry> list = new ArrayList<>();
+        blockTagKeys.forEach(tagKey -> list.add(TagEntry.tag(tagKey.location())));
+        return new MultiblockPart(name, list, minX, maxX, minY, maxY, minZ, maxZ);
+    }
 
     public BlockUtil.FoundRectangle getVerticalFace(BlockGetter blockGetter, BlockPos centerPos, Direction.Axis horizontalAxis) {
         return BlockUtil.getLargestRectangleAround(centerPos,
-                Direction.Axis.Y, maxY,
-                horizontalAxis, getAxisMax(horizontalAxis),
-                getBlockValidator(blockGetter, blocks));
+                Direction.Axis.Y, maxY/2,
+                horizontalAxis, getAxisMax(horizontalAxis)/2,
+                getBlockValidator(blockGetter, this.getValidBlocks()));
     }
+
+    public List<Block> getValidBlocks(){
+        List<Block> ret = new ArrayList<>();
+        this.entries.forEach(tagEntry -> tagEntry.build(blockTagEntryLookup, (ret::add)));
+        return ret;
+    }
+
+    private static final TagEntry.Lookup<Block> blockTagEntryLookup = new TagEntry.Lookup<>() {
+        @Override
+        @SuppressWarnings("deprecation")
+        public Block element(@NotNull ResourceLocation pElementLocation) {
+            return BuiltInRegistries.BLOCK.get(pElementLocation);
+        }
+
+        @SuppressWarnings("DataFlowIssue")
+        @Override
+        public List<Block> tag(@NotNull ResourceLocation pTagLocation) {
+            return ForgeRegistries.BLOCKS.tags().getTag(TagKey.create(Registries.BLOCK, pTagLocation)).stream().toList();
+        }
+    };
 
     public int getAxisMax(Direction.Axis axis) {
         return switch (axis) {
@@ -30,6 +70,7 @@ public record MultiblockPart(String name, List<Block> blocks,
         };
     }
 
+    @SuppressWarnings("unused")
     public int getAxisMin(Direction.Axis axis) {
         return switch (axis) {
             case X -> minX;
@@ -45,7 +86,7 @@ public record MultiblockPart(String name, List<Block> blocks,
     public static final Codec<MultiblockPart> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
                     Codec.STRING.fieldOf("name").forGetter(MultiblockPart::name),
-                    RecordCodecBuilder.of(MultiblockPart::blocks, ForgeRegistries.BLOCKS.getCodec().listOf().fieldOf("blocks")),
+                    TagEntry.CODEC.listOf().fieldOf("entries").forGetter(MultiblockPart::entries),
                     Codec.INT.fieldOf("minX").forGetter(MultiblockPart::minX),
                     Codec.INT.fieldOf("maxX").forGetter(MultiblockPart::maxX),
                     Codec.INT.fieldOf("minY").forGetter(MultiblockPart::minY),
