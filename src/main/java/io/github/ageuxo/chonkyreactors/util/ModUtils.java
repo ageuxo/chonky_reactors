7e188b941e122c48a759ca6e948dcbaeb3d773f9
ageuxo <ageuxo@gmail.com>
2023-08-06 21:41:56 +0200

    Remove unnecessary object creation in ModUtils.unpackColourValues

--- src/main/java/io/github/ageuxo/chonkyreactors/util/ModUtils.java
+++ src/main/java/io/github/ageuxo/chonkyreactors/util/ModUtils.java
@@ -14,20 +14,19 @@ public class ModUtils {
         return new ResourceLocation(ChonkyReactors.MODID, path);
     }
 
-    public static IntArray unpackColourValues(int packedColour){
-        IntArray array = new IntArray();
-        array.add(FastColor.ARGB32.alpha(packedColour));
-        array.add(FastColor.ARGB32.red(packedColour));
-        array.add(FastColor.ARGB32.green(packedColour));
-        array.add(FastColor.ARGB32.blue(packedColour));
-        return array;
+    public static int[] unpackColourValues(int packedColour){
+        return new int[]{ FastColor.ARGB32.alpha(packedColour),
+                            FastColor.ARGB32.red(packedColour),
+                            FastColor.ARGB32.green(packedColour),
+                            FastColor.ARGB32.blue(packedColour)
+                        };
     }
 
     public static void renderFluid(GuiGraphics guiGraphics, int x, int y, int width, int height, FluidStack fluidStack){
         if (!fluidStack.isEmpty()){
             Fluid fluid = fluidStack.getFluid();
             IClientFluidTypeExtensions clientType = IClientFluidTypeExtensions.of(fluid);
-            int[] colourValues = unpackColourValues(clientType.getTintColor()).data;
+            int[] colourValues = unpackColourValues(clientType.getTintColor());
             ResourceLocation texture = clientType.getStillTexture();
             guiGraphics.setColor(colourValues[0], colourValues[1], colourValues[2], colourValues[3]);
             guiGraphics.blit(texture, x, y, 0, 0, width, height);
